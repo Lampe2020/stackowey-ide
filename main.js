62f1924de6b3f6163e72d91daa7a5effc1871962
Lampe2020 <kontakt@lampe2020.de>
2025-04-11 12:26:30 +0200

    Add application stub

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-// Nothing to see here yet... 
+document.addEventListener('DOMContentLoaded', () => {
+    console.debug('Launching Stackowey IDE...');
+    document.querySelector('main').innerText = 'Loading...';
+    
+    try {
+        //...
+        throw new Error('The IDE is not implemented yet!');
+    } catch(err) {
+        document.querySelector('main').innerHTML = 'An unhandled error occurred and the IDE crashed!<br><br><details id="error"><summary id="error_summary"></summary><pre id="error_stacktrace"></pre></details>';
+        document.getElementById('error_summary').innerText = err.toString();
+        document.getElementById('error_stacktrace').innerText = err.stack;
+        window.stop();
+        throw err;
+    }
+});
