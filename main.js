f692fb7edbb7a29f9a4e0759c25364c3ed4a110d
Lampe2020 <kontakt@lampe2020.de>
2025-04-13 09:32:33 +0200

    Re-add missing `catch` block
    Why does my code so often just _disappear_ partially when I pluck the
    commits apart?!?

--- main.js
+++ main.js
@@ -30,6 +30,8 @@ document.addEventListener('DOMContentLoaded', () => {
         delete infoOpenBtn;
         
         throw new Error('The IDE is not implemented yet!');
+    } catch (err) {
+        (document.querySelector('main') || document.body).innerHTML = 'An unhandled error occurred and the IDE crashed!<br><br><details id="error"><summary id="error_summary"></summary><pre id="error_stacktrace"></pre></details>';
         document.getElementById('error_summary').innerText = err.toString();
         document.getElementById('error_stacktrace').innerText = err.stack;
         window.stop();
